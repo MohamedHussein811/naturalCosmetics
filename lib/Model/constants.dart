fb68bbd8dd54e633954918b443751dcea5b058f6
Mohamed <mohamed.hussein811811@gmail.com>
2023-12-22 15:15:54 +0200

    Hello1

---
+++ lib/Model/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+
+   const primaryColor = Color(0xFF0C9869);
+   const textColor = Color(0xFF3C4046);
+   const backgroundColor = Color(0xFFF9F8FD);
+   const backgroundColor2 = Color(0xFFF9F8FD);
+   const double DefaultPadding = 20.0;
+
