5be66e3df1445a43e6772979ae95bc29468a4532
Mohamed <mohamed.hussein811811@gmail.com>
2023-12-22 15:05:16 +0200

    Hello1

---
+++ lib/Model/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Colors {
+  static const primaryColor = Color(0xFF0C9869);
+  static const textColor = Color(0xFF3C4046);
+  static const backgroundColor = Color(0xFFF9F8FD);
+  static const backgroundColor2 = Color(0xFFF9F8FD);
+}
---
+++ lib/View/Screens/intro_screens/screen1.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Colors.indigoAccent, Colors.purpleAccent])),
+        ),
+      ),
+    );
+  }
+}
